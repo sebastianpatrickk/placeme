1460021dee976d759873827dd9df860dbba0c6ef
[sebastian-patrick-klen] <[sebastianklen.business@gmail.com]>
2023-02-26 17:20:17 +0100

    remove limiter

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '10kb' }));
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour!',
+// });
 app.use('/api', limiter);
 
 app.use(helmet());
